jcesarmobile/tea-taster-angular

step(eighteen): share the tasting notes

src/app/tasting-notes/tasting-note-editor/tasting-note-editor.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Plugins } from '@capacitor/core';
 import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 
 import { TastingNote, Tea } from '@app/models';
 import { selectTeas, State } from '@app/store';
-import { ModalController } from '@ionic/angular';
+import { ModalController, Platform } from '@ionic/angular';
 import { noteSaved } from '@app/store/actions';
 
 @Component({
@@ -31,8 +32,17 @@ export class TastingNoteEditorComponent implements OnInit {
     return this.note ? 'Update' : 'Add';
   }
 
+  get sharingIsAvailable(): boolean {
+    return this.platform.is('hybrid');
+  }
+
+  get allowSharing(): boolean {
+    return !!(this.brand && this.name && this.rating);
+  }
+
   constructor(
     private modalController: ModalController,
+    private platform: Platform,
     private store: Store<State>,
   ) {}
 
@@ -66,5 +76,15 @@ export class TastingNoteEditorComponent implements OnInit {
 
     this.store.dispatch(noteSaved({ note }));
     this.modalController.dismiss();
+  }
+
+  async share() {
+    const { Share } = Plugins;
+    await Share.share({
+      title: `${this.brand}: ${this.name}`,
+      text: `I gave ${this.brand}: ${this.name} ${this.rating} stars on the Tea Taster app`,
+      dialogTitle: 'Share your tasting note',
+      url: 'https://tea-taster-training.web.app',
+    });
   }
 }

jujasen/tea-taster-angular-ng12

src/app/tasting-notes/tasting-note-editor/tasting-note-editor.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Share } from '@capacitor/share';
 import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 
 import { TastingNote, Tea } from '@app/models';
 import { selectTeas, State } from '@app/store';
-import { ModalController } from '@ionic/angular';
+import { ModalController, Platform } from '@ionic/angular';
 import { noteSaved } from '@app/store/actions';
 
 @Component({
@@ -31,8 +32,17 @@ export class TastingNoteEditorComponent implements OnInit {
     return this.note ? 'Update' : 'Add';
   }
 
+  get sharingIsAvailable(): boolean {
+    return this.platform.is('hybrid');
+  }
+
+  get allowSharing(): boolean {
+    return !!(this.brand && this.name && this.rating);
+  }
+
   constructor(
     private modalController: ModalController,
+    private platform: Platform,
     private store: Store<State>,
   ) {}
 
@@ -66,5 +76,14 @@ export class TastingNoteEditorComponent implements OnInit {
 
     this.store.dispatch(noteSaved({ note }));
     this.modalController.dismiss();
+  }
+
+  async share() {
+    await Share.share({
+      title: `${this.brand}: ${this.name}`,
+      text: `I gave ${this.brand}: ${this.name} ${this.rating} stars on the Tea Taster app`,
+      dialogTitle: 'Share your tasting note',
+      url: 'https://tea-taster-training.web.app',
+    });
   }
 }